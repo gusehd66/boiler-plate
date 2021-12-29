fd51e481044b39308c79d567bfb4730a6dff2b3f
gusehd66 <rlagusehd45@naver.com>
2021-12-29 17:51:34 +0900

    :sparkles: Auth 인증 체크

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth";
 
 export default function App() {
+  const NewLandingPage = Auth(LandingPage, null);
+  const NewLoginPage = Auth(LoginPage, false);
+  const NewRegisterPage = Auth(RegisterPage, false);
+
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<NewRegisterPage />} />
+        <Route path="/login" element={<NewLoginPage />} />
+        <Route path="/" element={<NewLandingPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/hoc/auth.js
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../_actions/user_action";
+
+function Auth(SpecificComponent, option, adminRoute = null) {
+  //option
+  //null    =>  아무나 출입이 가능한 페이지
+  //true    =>  로그인한 유저만 출입이 가능한 페이지
+  //false   =>  로그인한 유저는 출입 불가능한 페이지
+  function AuthenticationCheck(props) {
+    const dispatch = useDispatch();
+    const history = useNavigate();
+
+    useEffect(() => {
+      dispatch(auth()).then((response) => {
+        console.log(response);
+        // 로그인 하지 않은 상태
+        if (!response.payload.isAuth) {
+          if (option) {
+            history("/login");
+          }
+        } else {
+          //로그인 한 상태
+          if (adminRoute && !response.payload.isAdmin) {
+            history("/");
+          } else {
+            if (option === false) history("/");
+          }
+        }
+      });
+    }, []);
+
+    return <SpecificComponent {...props} />;
+  }
+  return AuthenticationCheck;
+}
+
+export default Auth;
